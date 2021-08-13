20c2bfd1460a7cc7c74beb90fe3f7b2f41474e77
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-08-13 13:43:18 +0530

    [Ankit] Add: JSON from Inventory Object

---
+++ ObjectOrientedProgramming/InventoryManagement/InventoryMain.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ObjectOrientedProgramming.InventoryManagement
+{
+    public class InventoryMain
+    {
+        public void DisplayData(string filepath)
+        {
+            try
+            {
+                using (StreamReader read = new StreamReader(filepath))
+                {
+                    var json = read.ReadToEnd();
+
+                    var items = JsonConvert.DeserializeObject<List<InventoryModel>>(json);
+                    Console.WriteLine("Name\tWeight\tRate");
+                    foreach (var item in items)
+                    {
+                        Console.WriteLine(item.Name + "\t" + item.Weight + "\t" + item.PricePerKg);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+
+    }
+}
---
+++ ObjectOrientedProgramming/Program.cs
@@ -0,0 +1,15 @@
+﻿using ObjectOrientedProgramming.InventoryManagement;
+using System;
+
+namespace ObjectOrientedProgramming
+{
+    class Program
+    {
+        const string INVENTORY_JSON = @"F:\JSON\ObjectOrientedProgramming\InventoryManagement\Inventory.json";
+        static void Main(string[] args)
+        {
+            InventoryMain inventoryMain = new InventoryMain();
+            inventoryMain.DisplayData(INVENTORY_JSON);
+        }
+    }
+}
